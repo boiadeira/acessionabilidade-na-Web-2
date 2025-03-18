a960bbdb26de6d7377ad2a6ccafe0129721306d9
boiadeira <161638593+boiadeira@users.noreply.github.com>
2025-03-18 15:31:46 -0300

    18/03

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 document.addEventListener('DOMContentLoaded', function(){
 const aumetaFonteBotao=document.getElementById('aumentar-fonte');
+const diminuirFonteBotao=document.getElementById('diminuir-fonte');
 
 let tamanhoAtualFonte=1;
+
 aumetaFonteBotao.addEventListener('click', function(){
  tamanhoAtualFonte += 0.1;
  document.body.style.fontSize=`${tamanhoAtualFonte}rem`
+})
+diminuirFonteBotao.addEventListener('click', function(){
+    tamanhoAtualFonte += 0.1;
+    document.body.style.fontSize=`${tamanhoAtualFonte}rem`
 })
 })
