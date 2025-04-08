75e41ad974ce91c35f9f296669506871f0432fe0
boiadeira <161638593+boiadeira@users.noreply.github.com>
2025-04-08 16:08:51 -0300

    08

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-document.addEventListener('DOMContentLoaded', function(){
-    const botaoDeAcessibilidade= document.getElementById('botao-acessibilidade')
-    const opcoesDeAcessibilidade= document.getElementById('opcoes-acessibilidade')
-    const alternaContraste=document.getElementById('alterna-contraste')
+document.addEventListener('DOMContentLoaded', function () {
+  const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
+  const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
+  const alternaContraste = document.getElementById('alterna-contraste')
 
-     botaoDeAcessibilidade.addEventListener('click', function (){
-     botaoDeAcessibilidade.classList.toggle('rotacao-botao')
-     opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
-     
-})
+  botaoDeAcessibilidade.addEventListener('click', function () {
+    botaoDeAcessibilidade.classList.toggle('rotacao-botao')
+    opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
 
-     
-const aumetaFonteBotao=document.getElementById('aumentar-fonte');
-const diminuirFonteBotao=document.getElementById('diminuir-fonte');
+  })
 
-let tamanhoAtualFonte=1;
 
-aumetaFonteBotao.addEventListener('click', function(){
- tamanhoAtualFonte += 0.1;
- document.body.style.fontSize=`${tamanhoAtualFonte}rem`
-})
-diminuirFonteBotao.addEventListener('click', function(){
+  const aumetaFonteBotao = document.getElementById('aumentar-fonte');
+  const diminuirFonteBotao = document.getElementById('diminuir-fonte');
+
+  let tamanhoAtualFonte = 1;
+
+  aumetaFonteBotao.addEventListener('click', function () {
+    tamanhoAtualFonte += 0.1;
+    document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+  })
+  diminuirFonteBotao.addEventListener('click', function () {
     tamanhoAtualFonte -= 0.1;
-    document.body.style.fontSize=`${tamanhoAtualFonte}rem`
+    document.body.style.fontSize = `${tamanhoAtualFonte}rem`
 
-})
-alternaContraste.addEventListener('click',function(){
-  document.body,this.classList.toggle('auto-contraste')
-})
+  })
+  alternaContraste.addEventListener('click', function () {
+    document.body, this.classList.toggle('auto-contraste')
+  })
 })
