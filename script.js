d21f119edb0f56abc5bac80cffe4a85031a2e2d4
boiadeira <161638593+boiadeira@users.noreply.github.com>
2025-04-15 15:41:40 -0300

    15

--- script.js
+++ script.js
@@ -3,30 +3,34 @@ document.addEventListener('DOMContentLoaded', function () {
   const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
   const alternaContraste = document.getElementById('alterna-contraste')
 
+ 
+
   botaoDeAcessibilidade.addEventListener('click', function () {
-    botaoDeAcessibilidade.classList.toggle('rotacao-botao')
-    opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
-    const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded')=== 'true';
-    botaoDeAcessibilidade.setAttribute('aria-expanded',!botaoSelecionado)
+      botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+      opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
 
   })
 
-
-  const aumetaFonteBotao = document.getElementById('aumentar-fonte');
-  const diminuirFonteBotao = document.getElementById('diminuir-fonte');
+  const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+  const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
   let tamanhoAtualFonte = 1;
 
-  aumetaFonteBotao.addEventListener('click', function () {
-    tamanhoAtualFonte += 0.1;
-    document.body.style.fontSize = `${tamanhoAtualFonte}rem`
-  })
-  diminuirFonteBotao.addEventListener('click', function () {
-    tamanhoAtualFonte -= 0.1;
-    document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+  aumentaFonteBotao.addEventListener('click', function () {
+      tamanhoAtualFonte += 0.1;
+      document.body.style.fontSize = `${tamanhoAtualFonte}rem`
 
   })
-  alternaContraste.addEventListener('click', function () {
-    document.body, this.classList.toggle('auto-contraste')
+
+  diminuiFonteBotao.addEventListener('click', function () {
+      tamanhoAtualFonte -= 0.1;
+      document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+
   })
+
+  alternaContraste.addEventListener('click', function(){
+    document.body.classList.toggle('auto-contraste')
+})
+
+
 })
