e66c2a9cbc161cbac1efcde8987e16bb64520ca2
boiadeira <161638593+boiadeira@users.noreply.github.com>
2025-04-15 15:54:27 -0300

    15/04

--- script.js
+++ script.js
@@ -3,13 +3,10 @@ document.addEventListener('DOMContentLoaded', function () {
   const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
   const alternaContraste = document.getElementById('alterna-contraste')
 
- 
-
-  botaoDeAcessibilidade.addEventListener('click', function () {
-      botaoDeAcessibilidade.classList.toggle('rotacao-botao');
-      opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
-
-  })
+    botaoDeAcessibilidade.addEventListener('click', function () {
+    botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+    opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+ })
 
   const aumentaFonteBotao = document.getElementById('aumentar-fonte');
   const diminuiFonteBotao = document.getElementById('diminuir-fonte');
@@ -17,20 +14,19 @@ document.addEventListener('DOMContentLoaded', function () {
   let tamanhoAtualFonte = 1;
 
   aumentaFonteBotao.addEventListener('click', function () {
-      tamanhoAtualFonte += 0.1;
-      document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+    tamanhoAtualFonte += 0.1;
+    document.body.style.fontSize = `${tamanhoAtualFonte}rem`
 
   })
 
   diminuiFonteBotao.addEventListener('click', function () {
-      tamanhoAtualFonte -= 0.1;
-      document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+    tamanhoAtualFonte -= 0.1;
+    document.body.style.fontSize = `${tamanhoAtualFonte}rem`
 
   })
 
-  alternaContraste.addEventListener('click', function(){
+  alternaContraste.addEventListener('click', function () {
     document.body.classList.toggle('auto-contraste')
+  })
 })
-
-
-})
+ScrollReveal().reveal('#inicio', { delay: 500 });
